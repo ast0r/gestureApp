bea68c5ae8dc0416a5f57d715799af670eab2a04
Pavel Ivanov <ivanovpavel@Pavels-Mac.local>
2019-03-19 10:23:32 +0200

    Initial Commit

---
+++ GestureRecognizersApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GestureRecognizersApp
+//
+//  Created by Pavel Ivanov on 3/19/19.
+//  Copyright © 2019 Pavel Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
